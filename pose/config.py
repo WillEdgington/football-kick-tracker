6cd7c25320ac1fda4aa387fc7faa97af53e2f3e7
WillEdgington <willedge037@gmail.com>
2026-03-20 13:04:38 +0000

    feat: add pose constants and config modules

---
+++ pose/config.py
@@ -0,0 +1,10 @@
+CONFTHRESHOLD = 0.5
+
+LOWERCOLOURS = {
+    "left_hip": (255, 100, 0),
+    "right_hip": (255, 100, 0),
+    "left_knee": (0, 200, 255),
+    "right_knee": (0, 200, 255),
+    "left_ankle": (0, 255, 100),
+    "right_ankle": (0, 255, 100),
+}
---
+++ pose/constants.py
@@ -0,0 +1,15 @@
+LOWERKEYPOINTS = {
+    "left_hip": 11,
+    "right_hip": 12,
+    "left_knee": 13,
+    "right_knee": 14,
+    "left_ankle": 15,
+    "right_ankle": 16,
+}
+
+LOWERSKELETONPAIRS = [
+    ("left_hip", "left_knee"),
+    ("left_knee", "left_ankle"),
+    ("right_hip", "right_knee"),
+    ("right_knee", "right_ankle"),
+]
