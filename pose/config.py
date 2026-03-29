b232fe22a2c30bfc3151cfcb18e35792326035a9
WillEdgington <willedge037@gmail.com>
2026-03-29 19:06:30 +0100

    feat: (pose) add default composite score weights and relevant relative paths to config.py

--- pose/config.py
+++ pose/config.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 CONFTHRESHOLD = 0.5
 
 LOWERCOLOURS = {
@@ -8,3 +10,14 @@ LOWERCOLOURS = {
     "left_ankle": (0, 255, 100),
     "right_ankle": (0, 255, 100),
 }
+
+COMPSCOREWEIGHTS = {
+    "detection_rate": 0.5,
+    "left_ankle_conf": 0.15,
+    "right_ankle_conf": 0.15,
+    "left_knee_conf": 0.1,
+    "right_knee_conf": 0.1,
+}
+
+RAWTRAININGVIDEOSDIR = Path("data/raw/training_drills_videos")
+ANNOTATEDPOSEVIDEOSDIR = Path("data/sessions/annotated_videos")
