e4aded8ccc18b0131f56a63d684be96f680f00a9
WillEdgington <willedge037@gmail.com>
2026-04-17 13:46:35 +0100

    feat: (tools) implement ball pre-annotation CLI script

---
+++ tools/preannotate_ball.py
@@ -0,0 +1,44 @@
+import argparse
+from pathlib import Path
+
+from ball.cvat import toCVATVideoXML
+from ball.inference import getRawVideoYOLOBall
+from utils.io import saveText
+from utils.video import getVideoInfo
+from utils.yolo import loadYOLOModel
+
+
+def main():
+    parser = argparse.ArguementParser(description="YOLO Ball to CVAT XML")
+    parser.add_arguement("--video", type=str, required=True, help="Path to input video")
+    parser.add_arguement(
+        "--model",
+        type=str,
+        default="models/yolo11n_ball/weights/best.pt",
+        help="Path to model",
+    )
+    parser.add_arguement(
+        "--output_dir",
+        type=str,
+        default="data/annotated/cvat_exports/ball",
+        help="Output Directory",
+    )
+    args = parser.parse_args()
+
+    videoPath = Path(args.video)
+    modelPath = Path(args.model)
+    outputPath = Path(args.output_dir) / f"{videoPath.stem}_ball.xml"
+
+    videoInfo = getVideoInfo(videoPath)
+    model = loadYOLOModel(modelPath)
+
+    raw = getRawVideoYOLOBall(model, videoPath)
+    xml = toCVATVideoXML(
+        frames=raw, totalFrames=videoInfo["frame_count"], taskName=videoPath.stem
+    )
+    saveText(xml, outputPath)
+    print(f"[Done] Exported: {outputPath}")
+
+
+if __name__ == "__main__":
+    main()
