09a638d67652963b4a06b916b2e2df2e0ef7e200
WillEdgington <willedge037@gmail.com>
2026-04-20 13:29:59 +0100

    feat: (utils) getAllVideoPaths method for collecting all video paths from input directory

--- utils/video.py
+++ utils/video.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 import cv2
 
@@ -17,3 +17,22 @@ def getVideoInfo(path: Path) -> Dict[str, Any]:
     }
     cap.release()
     return info
+
+
+def getAllVideoPaths(
+    videoDir: Path, mp4: bool = True, mov: bool = True, prefix: str | None = None
+) -> List[Path]:
+    assert mp4 or mov, "mp4 or mov must be set to True"
+    assert videoDir.is_dir(), "videoDir must be a valid directory"
+    suffixes = []
+    if mp4:
+        suffixes.append("*.mp4")
+    if mov:
+        suffixes.append("*.mov")
+
+    videoPaths = []
+    for suf in suffixes:
+        videoPaths.extend(videoDir.glob(suf))
+    if prefix is not None:
+        videoPaths = [p for p in videoPaths if p.stem.startswith(prefix)]
+    return videoPaths
